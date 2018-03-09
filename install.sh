28d0c4b83de27b5ffe62a28489cfecfa3d284b2b
Juan Jimeno <jimenojmm@gmail.com>
2018-03-09 17:01:32 +0800

    added libeigen3-dev

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get install -y ros-indigo-desktop-full ros-indigo-nmea-msgs ros-indigo-nmea-navsat-driver ros-indigo-sound-play ros-indigo-jsk-visualization
 sudo apt-get install -y libnlopt-dev freeglut3-dev qtbase5-dev libqt5opengl5-dev libssh2-1-dev libarmadillo-dev libpcap-dev gksu libgl1-mesa-dev
+sudo apt-get install -y libeigen3-dev
 
 sudo apt-get install -y ros-indigo-controller-manager
 sudo apt-get install -y libglew-dev
@@ -19,17 +20,17 @@ catkin_init_workspace
 cd ../
 ./catkin_make_release
 
-echo "alias Autoware=~/Autoware/ros/./run" >> ~/.bashrc
+# echo "alias Autoware=~/Autoware/ros/./run" >> ~/.bashrc
 
-cd ~/
-mkdir .autoware -p && cd .autoware
-wget http://db3.ertl.jp/autoware/sample_data/my_launch.sh
-wget http://db3.ertl.jp/autoware/sample_data/sample_moriyama_data.tar.gz
-wget http://db3.ertl.jp/autoware/sample_data/sample_moriyama_150324.tar.gz
+# cd ~/
+# mkdir .autoware -p && cd .autoware
+# wget http://db3.ertl.jp/autoware/sample_data/my_launch.sh
+# wget http://db3.ertl.jp/autoware/sample_data/sample_moriyama_data.tar.gz
+# wget http://db3.ertl.jp/autoware/sample_data/sample_moriyama_150324.tar.gz
 
-tar xfz sample_moriyama_data.tar.gz
-tar xfz sample_moriyama_150324.tar.gz
+# tar xfz sample_moriyama_data.tar.gz
+# tar xfz sample_moriyama_150324.tar.gz
 
-sh my_launch.sh
+# sh my_launch.sh
 
 
