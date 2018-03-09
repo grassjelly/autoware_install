e8006dfe2e6d0fa5e302333c4b1468d502b3f8df
Juan Jimeno <jimenojmm@gmail.com>
2018-03-09 17:24:52 +0800

    changed installation

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
-sudo apt-get install -y ros-indigo-desktop-full ros-indigo-nmea-msgs ros-indigo-nmea-navsat-driver ros-indigo-sound-play ros-indigo-jsk-visualization
-sudo apt-get install -y libnlopt-dev freeglut3-dev qtbase5-dev libqt5opengl5-dev libssh2-1-dev libarmadillo-dev libpcap-dev gksu libgl1-mesa-dev
-sudo apt-get install -y libeigen3-dev
+# sudo apt-get install -y ros-indigo-desktop-full ros-indigo-nmea-msgs ros-indigo-nmea-navsat-driver ros-indigo-sound-play ros-indigo-jsk-visualization
+# sudo apt-get install -y libnlopt-dev freeglut3-dev qtbase5-dev libqt5opengl5-dev libssh2-1-dev libarmadillo-dev libpcap-dev gksu libgl1-mesa-dev
+# # sudo apt-get install -y libeigen3-dev
 
-sudo apt-get install -y ros-indigo-controller-manager
-sudo apt-get install -y libglew-dev
-sudo apt-get install -y ros-indigo-grid-map-ros
-sudo apt-get install -y ros-indigo-controller-manager
-sudo apt-get install -y ros-indigo-gazebo-ros-control
-sudo apt-get install -y ros-indigo-position-controllers
-sudo apt-get install -y ros-indigo-sicktoolbox
-sudo apt-get install -y ros-indigo-sicktoolbox-wrapper
-sudo apt-get install -y ros-indigo-velocity-controllers
+# sudo apt-get install -y ros-indigo-eigen3
+# sudo apt-get install -y ros-indigo-controller-manager
+# sudo apt-get install -y libglew-dev
+# sudo apt-get install -y ros-indigo-grid-map-ros
+# sudo apt-get install -y ros-indigo-controller-manager
+# sudo apt-get install -y ros-indigo-gazebo-ros-control
+# sudo apt-get install -y ros-indigo-position-controllers
+# sudo apt-get install -y ros-indigo-sicktoolbox
+# sudo apt-get install -y ros-indigo-sicktoolbox-wrapper
+# sudo apt-get install -y ros-indigo-velocity-controllers
+
+
+sudo apt-get install  -y ros-indigo-desktop-full ros-indigo-nmea-msgs ros-indigo-nmea-navsat-driver ros-indigo-sound-play ros-indigo-jsk-visualization ros-indigo-grid-map ros-indigo-gps-common
+sudo apt-get install -y ros-indigo-controller-manager ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-gazebo-ros-control ros-indigo-sicktoolbox ros-indigo-sicktoolbox-wrapper ros-indigo-joystick-drivers ros-indigo-novatel-span-driver
+sudo apt-get install -y libnlopt-dev freeglut3-dev qtbase5-dev libqt5opengl5-dev libssh2-1-dev libarmadillo-dev libpcap-dev gksu libgl1-mesa-dev libglew-dev software-properties-common libyaml-cpp-dev python-flask python-requests
+sudo add-apt-repository ppa:mosquitto-dev/mosquitto-ppa
+sudo apt-get install -y libmosquitto-dev
 
 cd ~/
 git clone https://github.com/CPFL/Autoware.git
@@ -32,5 +40,4 @@ cd ../
 # tar xfz sample_moriyama_150324.tar.gz
 
 # sh my_launch.sh
-
 
