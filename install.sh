354aa3933fa7d7b5e2e151d09eb51d0f4ddd71b4
Juan Jimeno <jimenojmm@gmail.com>
2018-03-16 03:41:08 +0800

    test8

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
-find /usr/share -name "FindEigen3.cmake"
-ls /usr/share/cmake*
-sudo apt-get install -y libeigen3-dev
-cp $(find /usr/share -name "FindEigen3.cmake") /usr/share/usr/share/cmake-3.2/Modules/
-
-# sudo apt-get install -y ros-indigo-desktop-full ros-indigo-nmea-msgs ros-indigo-nmea-navsat-driver ros-indigo-sound-play ros-indigo-jsk-visualization ros-indigo-grid-map ros-indigo-gps-common
-# sudo apt-get install -y ros-indigo-controller-manager ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-gazebo-ros-control ros-indigo-sicktoolbox ros-indigo-sicktoolbox-wrapper ros-indigo-joystick-drivers ros-indigo-novatel-span-driver
-# sudo apt-get install -y libnlopt-dev freeglut3-dev qtbase5-dev libqt5opengl5-dev libssh2-1-dev libarmadillo-dev libpcap-dev gksu libgl1-mesa-dev libglew-dev software-properties-common libyaml-cpp-dev python-flask python-requests
+# find /usr/share -name "FindEigen3.cmake"
+# ls /usr/share/cmake*
 # sudo apt-get install -y libeigen3-dev
+# cp $(find /usr/share -name "FindEigen3.cmake") /usr/share/usr/share/cmake-3.2/Modules/
 
-# yes | sudo add-apt-repository ppa:mosquitto-dev/mosquitto-ppa
-# sudo apt-get install -y libmosquitto-dev
+sudo apt-get install -y ros-indigo-desktop-full ros-indigo-nmea-msgs ros-indigo-nmea-navsat-driver ros-indigo-sound-play ros-indigo-jsk-visualization ros-indigo-grid-map ros-indigo-gps-common
+sudo apt-get install -y ros-indigo-controller-manager ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-gazebo-ros-control ros-indigo-sicktoolbox ros-indigo-sicktoolbox-wrapper ros-indigo-joystick-drivers ros-indigo-novatel-span-driver
+sudo apt-get install -y libnlopt-dev freeglut3-dev qtbase5-dev libqt5opengl5-dev libssh2-1-dev libarmadillo-dev libpcap-dev gksu libgl1-mesa-dev libglew-dev software-properties-common libyaml-cpp-dev python-flask python-requests
+sudo apt-get install -y ros-indigo-pcl-ros
 
-# cd ~/
-# git clone https://github.com/CPFL/Autoware.git
-# git checkout devel
-# cd ~/Autoware/ros/src
-# catkin_init_workspace
-# cd ../
+yes | sudo add-apt-repository ppa:mosquitto-dev/mosquitto-ppa
+sudo apt-get install -y libmosquitto-dev
 
-# ./catkin_make_release
+cd ~/
+git clone https://github.com/CPFL/Autoware.git
+git checkout devel
+cd ~/Autoware/ros/src
+catkin_init_workspace
+cd ../
 
-# echo "alias Autoware=~/Autoware/ros/./run" >> ~/.bashrc
+./catkin_make_release
+
+echo "alias Autoware=~/Autoware/ros/./run" >> ~/.bashrc
 
 # cd ~/
 # mkdir .autoware -p && cd .autoware
